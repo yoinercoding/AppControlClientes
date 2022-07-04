56ded0f4d0ecf78cd2cf6a67116efac30eb8715e
ybello <yoinercoding@gmail.com>
2022-07-04 11:51:52 -0300

    Se agrega Post Guardar

--- src/main/java/app/control/clientes/web/ControladorInicio.java
+++ src/main/java/app/control/clientes/web/ControladorInicio.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -27,4 +28,11 @@ public class ControladorInicio {
     public String agregar(Persona persona) {
         return "modificar";
     }
+
+    @PostMapping("/guardar")
+        public String guardar(Persona persona) {
+           personaService.guardar(persona);
+           return "redirect:/";
+    }
+
 }
