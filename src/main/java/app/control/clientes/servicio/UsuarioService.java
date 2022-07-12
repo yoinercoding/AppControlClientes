c2b0e6b772799f82f4e7134fe97d67c8db96b98a
ybello <yoinercoding@gmail.com>
2022-07-12 10:02:24 -0300

    Creacion UsuarioService

---
+++ src/main/java/app/control/clientes/servicio/UsuarioService.java
@@ -0,0 +1,41 @@
+package app.control.clientes.servicio;
+
+import app.control.clientes.dao.UsuarioDao;
+import app.control.clientes.domain.Rol;
+import app.control.clientes.domain.Usuario;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service("userDetailsService")
+@Slf4j
+public class UsuarioService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioDao.findByUsername(username);
+
+        if(usuario == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        var roles = new ArrayList<GrantedAuthority>();
+
+        for(Rol rol: usuario.getRoles()) {
+            roles.add(new SimpleGrantedAuthority(rol.getNombre()));
+        }
+
+        return new User(usuario.getUsername(), usuario.getPassword(), roles);
+    }
+}
